3267aadbc1d03c8e8d678502770dd04f31a85d93
Deekshitha Pedasingu <pedasingudeekshitha0@gmail.com>
2026-05-23 16:20:55 +0530

    Create BankManagementSystem.java

---
+++ BankManagementSystem.java
@@ -0,0 +1,105 @@
+import java.util.Scanner;
+
+class BankAccount {
+
+    int accountNumber;
+    String accountHolder;
+    double balance;
+
+    BankAccount(int accountNumber, String accountHolder, double balance) {
+        this.accountNumber = accountNumber;
+        this.accountHolder = accountHolder;
+        this.balance = balance;
+    }
+
+    void deposit(double amount) {
+        balance += amount;
+        System.out.println("Amount Deposited Successfully!");
+    }
+
+    void withdraw(double amount) {
+
+        if (amount > balance) {
+            System.out.println("Insufficient Balance!");
+        } else {
+            balance -= amount;
+            System.out.println("Withdrawal Successful!");
+        }
+    }
+
+    void displayAccount() {
+
+        System.out.println("\n===== Account Details =====");
+        System.out.println("Account Number: " + accountNumber);
+        System.out.println("Account Holder: " + accountHolder);
+        System.out.println("Balance: ₹" + balance);
+    }
+}
+
+public class BankManagementSystem {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("===== Create Bank Account =====");
+
+        System.out.print("Enter Account Number: ");
+        int accNo = sc.nextInt();
+        sc.nextLine();
+
+        System.out.print("Enter Account Holder Name: ");
+        String name = sc.nextLine();
+
+        System.out.print("Enter Initial Balance: ");
+        double balance = sc.nextDouble();
+
+        BankAccount user = new BankAccount(accNo, name, balance);
+
+        while (true) {
+
+            System.out.println("\n===== Bank Management System =====");
+            System.out.println("1. Deposit");
+            System.out.println("2. Withdraw");
+            System.out.println("3. View Account");
+            System.out.println("4. Exit");
+
+            System.out.print("Enter Your Choice: ");
+            int choice = sc.nextInt();
+
+            switch (choice) {
+
+                case 1:
+
+                    System.out.print("Enter Deposit Amount: ");
+                    double depositAmount = sc.nextDouble();
+
+                    user.deposit(depositAmount);
+                    break;
+
+                case 2:
+
+                    System.out.print("Enter Withdraw Amount: ");
+                    double withdrawAmount = sc.nextDouble();
+
+                    user.withdraw(withdrawAmount);
+                    break;
+
+                case 3:
+
+                    user.displayAccount();
+                    break;
+
+                case 4:
+
+                    System.out.println("Exiting Program...");
+                    sc.close();
+                    return;
+
+                default:
+
+                    System.out.println("Invalid Choice!");
+            }
+        }
+    }
+  }
